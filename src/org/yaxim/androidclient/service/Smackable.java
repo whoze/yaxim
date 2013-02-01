3f7fe347ebbc3fc226c84a0857024c9954dfb1fb
Dario Ernst <daddel9@nebuk.de>
2013-02-01 12:25:54 +0100

    added code for muc, untested

--- src/org/yaxim/androidclient/service/Smackable.java
+++ src/org/yaxim/androidclient/service/Smackable.java
@@ -30,6 +30,9 @@ public interface Smackable {
 	void unRegisterCallback();
 	
 	void mucTest();
+	boolean joinRoom(String room, String nickname, String password, int historyLen);
+	String[] getJoinedRooms();
+	boolean createRoom(String room, String nickname, String password);
 	
 	String getNameForJID(String jid);
 }
