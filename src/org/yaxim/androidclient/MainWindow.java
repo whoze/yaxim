f6dda4a167fa83ed59d0505ee46469bfe170952b
Dario Ernst <daddel9@nebuk.de>
2013-06-15 11:48:11 +0200

    now also mark all as read

--- src/org/yaxim/androidclient/MainWindow.java
+++ src/org/yaxim/androidclient/MainWindow.java
@@ -555,6 +555,14 @@ public class MainWindow extends SherlockExpandableListActivity {
 		return false;
 	}
 
+	private void markAllAsRead() {
+		ContentValues cv = new ContentValues();
+		cv.put(ChatConstants.DELIVERY_STATUS, ChatConstants.DS_SENT_OR_READ);
+		getContentResolver().update(ChatProvider.CONTENT_URI, cv,
+						ChatConstants.DIRECTION + " = " + ChatConstants.INCOMING + " AND "
+						+ ChatConstants.DELIVERY_STATUS + " = " + ChatConstants.DS_NEW, null);
+	}
+	
 	private boolean isChild(long packedPosition) {
 		int type = ExpandableListView.getPackedPositionType(packedPosition);
 		return (type == ExpandableListView.PACKED_POSITION_TYPE_CHILD);
@@ -728,6 +736,10 @@ public class MainWindow extends SherlockExpandableListActivity {
 		case R.id.menu_show_hide:
 			setOfflinceContactsVisibility(!mConfig.showOffline);
 			updateRoster();
+			return true;
+			
+		case R.id.menu_markallread:
+			markAllAsRead();
 			return true;
 
 		case android.R.id.home:
