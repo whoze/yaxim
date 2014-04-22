092285ae0b2e864df31f3bc084d5117eb5fa3ed8
Georg Lukas <georg@op-co.de>
2014-04-22 15:34:18 +0200

    MUC: sort participant list alphabetically

--- src/org/yaxim/androidclient/service/SmackableImp.java
+++ src/org/yaxim/androidclient/service/SmackableImp.java
@@ -1555,6 +1555,7 @@ public class SmackableImp implements Smackable {
 		ArrayList<String> tmpList = new ArrayList<String>();
 		while(occIter.hasNext())
 			tmpList.add(occIter.next().split("/")[1]);
+		Collections.sort(tmpList, java.text.Collator.getInstance());
 		return (String[]) tmpList.toArray(new String[]{});
 	}
 }
