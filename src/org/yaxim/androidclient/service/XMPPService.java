eca2fdaee48a57500aa0cb4e57977b4b1be3b611
Georg Lukas <georg@op-co.de>
2014-04-22 15:58:31 +0200

    fixup bdbc59e1: from array

--- src/org/yaxim/androidclient/service/XMPPService.java
+++ src/org/yaxim/androidclient/service/XMPPService.java
@@ -539,7 +539,7 @@ public class XMPPService extends GenericService {
 		mSmackable.registerCallback(new XMPPServiceCallback() {
 			public void newMessage(String[] from, String message, boolean silent_notification, Type msgType) {
 				logInfo("notification: " + from +" with type: "+msgType.name());
-				notifyClient(from, mSmackable.getNameForJID(from[0]), message, !mIsBoundTo.contains(from), false, silent_notification, msgType);
+				notifyClient(from, mSmackable.getNameForJID(from[0]), message, !mIsBoundTo.contains(from[0]), false, silent_notification, msgType);
 			}
 
 			public void messageError(final String[] from, final String error, final boolean silent_notification) {
