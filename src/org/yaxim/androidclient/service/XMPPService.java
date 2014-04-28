7da769d160536e5995676e01c1647bf110ff1b4c
Georg Lukas <georg@op-co.de>
2014-04-28 07:05:07 +0200

    service: override presence with 'dnd' when silent

--- src/org/yaxim/androidclient/service/XMPPService.java
+++ src/org/yaxim/androidclient/service/XMPPService.java
@@ -18,6 +18,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Handler;
@@ -36,6 +37,7 @@ public class XMPPService extends GenericService {
 	private Intent mAlarmIntent = new Intent(RECONNECT_ALARM);
 	private PendingIntent mPAlarmIntent;
 	private BroadcastReceiver mAlarmReceiver = new ReconnectAlarmReceiver();
+	private BroadcastReceiver mRingerModeReceiver = new RingerModeReceiver();
 
 	private ServiceNotification mServiceNotification = null;
 
@@ -90,6 +92,8 @@ public class XMPPService extends GenericService {
 		mPAlarmIntent = PendingIntent.getBroadcast(this, 0, mAlarmIntent,
 					PendingIntent.FLAG_UPDATE_CURRENT);
 		registerReceiver(mAlarmReceiver, new IntentFilter(RECONNECT_ALARM));
+		registerReceiver(mRingerModeReceiver, new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION));
+		configureSmartAwayMode();
 
 		YaximBroadcastReceiver.initNetworkStatus(getApplicationContext());
 
@@ -116,6 +120,7 @@ public class XMPPService extends GenericService {
 		    mSmackable.unRegisterCallback();
 		}
 		unregisterReceiver(mAlarmReceiver);
+		unregisterReceiver(mRingerModeReceiver);
 	}
 
 	@Override
@@ -490,6 +495,24 @@ public class XMPPService extends GenericService {
 				return;
 			}
 			doConnect();
+		}
+	}
+
+	private void configureSmartAwayMode() {
+		AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+		boolean is_silent = (am.getRingerMode() == AudioManager.RINGER_MODE_SILENT);
+		mConfig.smartAwayMode = is_silent ? StatusMode.dnd : null;
+		logInfo("configureSmartAwayMode: " + mConfig.smartAwayMode);
+	}
+
+	private class RingerModeReceiver extends BroadcastReceiver {
+		public void onReceive(Context ctx, Intent i) {
+			logInfo("Ringer mode changed: " + i);
+			configureSmartAwayMode();
+			if (mSmackable != null && mSmackable.isAuthenticated()) {
+				mSmackable.setStatusFromConfig();
+				updateServiceNotification();
+			}
 		}
 	}
 }
