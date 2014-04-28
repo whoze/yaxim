52d6689d7928c13c0bf9462f74e2f4e870c7b95e
Georg Lukas <georg@op-co.de>
2014-04-28 23:10:50 +0200

    smart away: reset smartaway on manual status change

--- src/org/yaxim/androidclient/MainWindow.java
+++ src/org/yaxim/androidclient/MainWindow.java
@@ -588,6 +588,8 @@ public class MainWindow extends SherlockExpandableListActivity {
 		}
 		prefedit.putString(PreferenceConstants.STATUS_MESSAGE, message);
 		prefedit.commit();
+		// on a manual status update, reset auto-away
+		mConfig.smartAwayMode = null;
 
 		displayOwnStatus();
 
