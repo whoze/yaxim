9a923343e1b3b64a2e743117f93b98957eedba08
Georg Lukas <georg@op-co.de>
2014-07-12 20:41:01 +0200

    remove backend isRoom method

--- src/org/yaxim/androidclient/service/SmackableImp.java
+++ src/org/yaxim/androidclient/service/SmackableImp.java
@@ -1466,11 +1466,6 @@ public class SmackableImp implements Smackable {
 	}
 
 	@Override
-	public boolean isRoom(String jid) {
-		return mucJIDs.contains(jid);
-	}
-
-	@Override
 	public boolean inviteToRoom(String contactJid, String roomJid) {
 		MultiUserChat muc = multiUserChats.get(roomJid);
 		if(contactJid.contains("/")) {
