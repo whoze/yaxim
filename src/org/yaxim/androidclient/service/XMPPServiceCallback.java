17f2fd2bebde2f80e672db71d86c4fc96ca9da4a
Georg Lukas <georg@op-co.de>
2014-07-13 23:45:22 +0200

    service: removed no-op rosterChanged() callback

--- src/org/yaxim/androidclient/service/XMPPServiceCallback.java
+++ src/org/yaxim/androidclient/service/XMPPServiceCallback.java
@@ -6,6 +6,5 @@ public interface XMPPServiceCallback {
 	void newMessage(String[] from, String messageBody, boolean silent_notification, Message.Type msgType);
 	void messageError(String[] from, String errorBody, boolean silent_notification);
 	void connectionStateChanged();
-	void rosterChanged(); // TODO: remove that!
 	void mucInvitationReceived(String room, String body);
 }
